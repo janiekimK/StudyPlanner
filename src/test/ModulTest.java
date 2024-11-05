7b2cff566f786c761d8ed24db367f4612016801a
janiekimK <janie.kabashi@bbzbl-it.ch>
2024-11-05 15:52:26 +0100

    Modul Tests

--- src/test/ModulTest.java
+++ src/test/ModulTest.java
@@ -1,6 +1,41 @@
- private Modul mockedModule
+private Modul mockedModul;
 
- @BeforeEach
+@BeforeEach
+// Mock Objekt von Modul
 void setUp() {
         mockedModul = Mockito.spy(new Modul("Testmodul", 5));
+        }
+
+@Test
+// Testet ob getId den erwarteten Wert gibt
+void testGetId() {
+        when(mockedModul.getId()).thenReturn("123456");
+        assertEquals("123456", mockedModul.getId());
+        }
+
+@Test
+// Testet ob getUuid die erwartete Liste zurückgibt
+void testGetUuid() {
+        List<String> expectedUuids = Arrays.asList("ListTest1", "ListTest2", "ListTest3");
+        when(mockedModul.getUuid()).thenReturn(expectedUuids);
+        assertEquals(expectedUuids, mockedModul.getUuid());
+        }
+
+@Test
+// Testet ob GetModulname den erwarteten Modulnamen zurückgibt
+void testGetModulname() {
+        assertEquals("Testmodul1", mockedModul.getModulname());
+        }
+
+@Test
+// Testet ob setModulname() den Modulnamen setzt und die getModulname() den neuen Wert zurückgibt
+void testSetModulname() {
+        mockedModul.setModulname("Testmodulname");
+        assertEquals("Testmodulname", mockedModul.getModulname());
+        }
+
+@Test
+// Testet ob gettEcts() den erwarteten Wert für die ECTS-Punkte zurückgibt
+void testGetEcts() {
+        assertEquals(5, mockedModul.gettEcts());
         }
